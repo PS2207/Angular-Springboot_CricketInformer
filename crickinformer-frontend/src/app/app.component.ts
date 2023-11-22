ec3ffc7f48d37e2a879cf6c0e4c9793cc664e0a3
PS2207 <pragyasinha2207@gmail.com>
2023-11-22 20:05:48 +0530

    Create app.component.ts

---
+++ crickinformer-frontend/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component,  } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  //Adding RouterModule for routing
+  imports: [CommonModule, RouterOutlet,RouterModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  
+  title = 'crickinformer-frontend';
+  
+  isMobileMenuOpen = false;
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+
+}
